e6a2a89c8db1ad34b7280e3209c36975cca9969f
Akshay <akshay@myvaxa.com>
2019-08-07 11:47:23 +0530

    template driven forms changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forms';
+  gender = ['male', 'female'];
+  @ViewChild('f') detailsForm: NgForm;
+
+  onSubmit() {
+    console.log(this.detailsForm)
+  }
 }
